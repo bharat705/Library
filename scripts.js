ccc76d72b0b67669815ebe49650ce2f25b54dc74
bharat705 <73097406+bharat705@users.noreply.github.com>
2024-10-15 09:25:38 +0545

    add dynamic output divs using javascript Changes to be committed: 	modified:   index.html 	modified:   scripts.js

--- scripts.js
+++ scripts.js
@@ -37,36 +37,89 @@ const library = [
   },
 ];
 
-function Book(
-  title = "xyz",
-  author = "abc",
-  numberOfPages = "32",
-  hasRead = true
-) {
+function Book(title, author, numberOfPages, hasRead) {
   this.title = title;
   this.author = author;
   this.numberOfPages = numberOfPages;
   this.hasRead = hasRead;
 }
 
-function addBookToLibrary() {
-  let newBook = new Book();
+function addBookToLibrary(title, author, numberOfPages, hasRead) {
+  let newBook = new Book(title, author, numberOfPages, hasRead);
   library.push(newBook);
 }
 
-addBookToLibrary();
-console.log(library);
-
 const dialog = document.querySelector("dialog");
-const submitButton = document.querySelector("#submit-button");
+const inputForm = document.querySelector("#input-form");
 const addBookButton = document.querySelector("#add-book-button");
+const submitButton = document.querySelector("#submit-button");
+const cancelButton = document.querySelector("#cancel-button");
 
 // "Show the dialog" button opens the dialog modally
 addBookButton.addEventListener("click", () => {
   dialog.showModal();
 });
 
-// "Show the dialog" button opens the dialog modally
-submitButton.addEventListener("click", () => {
+cancelButton.addEventListener("click", (event) => {
   dialog.close();
 });
+
+dialog.addEventListener("close", (e) => {
+  console.log(dialog.returnValue);
+  inputForm.reset();
+});
+
+inputForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (inputForm.checkValidity()) {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const numberOfPages = document.getElementById("no-of-pages").value;
+    const hasRead = document.getElementById("read-status").checked
+      ? true
+      : false;
+
+    // Output the form values
+    addBookToLibrary(title, author, numberOfPages, hasRead);
+    displayCards();
+
+    dialog.close();
+    inputForm.reset();
+  } else {
+    inputForm.reportValidity();
+  }
+});
+
+const cardsContainer = document.querySelector("#cards-container");
+
+function displayCards() {
+  cardsContainer.innerHTML = "";
+  library.forEach((book) => {
+    let cardsDiv = document.createElement("div");
+    cardsDiv.className = "cards";
+    cardsContainer.appendChild(cardsDiv);
+
+    let titleDiv = document.createElement("div");
+    titleDiv.className = "title";
+    titleDiv.textContent = book.title;
+    cardsDiv.appendChild(titleDiv);
+
+    let authorDiv = document.createElement("div");
+    authorDiv.className = "author";
+    authorDiv.textContent = book.author;
+    cardsDiv.appendChild(authorDiv);
+
+    let pagesDiv = document.createElement("div");
+    pagesDiv.className = "no-of-pages";
+    pagesDiv.textContent = book.numberOfPages;
+    cardsDiv.appendChild(pagesDiv);
+
+    let hasReadDiv = document.createElement("div");
+    hasReadDiv.className = "read-status";
+    hasReadDiv.textContent = book.hasRead
+      ? "Read Already"
+      : "Yet to read";
+    cardsDiv.appendChild(hasReadDiv);
+  });
+}
